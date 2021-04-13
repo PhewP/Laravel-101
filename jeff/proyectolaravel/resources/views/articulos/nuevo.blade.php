6f506ecb66e966eaffffb737fa0051b7c97e2c35
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-13 13:54:43 +0200

    :sparkles: add nuevo view

---
+++ jeff/proyectolaravel/resources/views/articulos/nuevo.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('contenido')
+<h2>Insertar articulo</h2>
+<a href="{{route('articulos')}}" title="Articulos"> Articulos</a>
+<!-- Abrimos el formlario que se enviara a la ruta guardar articulo -->
+<form action="{{route('guardar_articulo')}}" method="POST">
+{{csrf_field()}}
+<!-- Para insertar un texto con un name= titulo -->
+<label for="titulo">Titulo:</label> <br/>
+<input type="text" name="titulo" class="input" value=""> <br/><br/>
+<!-- Para insertar un texto con un name descripcion -->
+<label for="descripcion">Descripcion:</label> <br/>
+<textarea name="descripcion" rows="5" class="form-control"></textarea>
+<br/><br/>
+<!-- Para insertar un texto con un name cuerpo -->
+<label for="cuerpo">Cuerpo:</label><br/>
+<textarea name="cuerpo" rows="8" class="form-control"></textarea>
+<br /> <br />
+<button type="submit" class="btn btn-primary">Crear Articulo</button>
+</form>
+@endsection
