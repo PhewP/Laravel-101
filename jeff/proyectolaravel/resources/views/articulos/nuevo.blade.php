647bf84721375c5ef9748c85467d66a8e798838f
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-13 15:22:10 +0200

    :recycle: mod nuevo view with validation messages

--- jeff/proyectolaravel/resources/views/articulos/nuevo.blade.php
+++ jeff/proyectolaravel/resources/views/articulos/nuevo.blade.php
@@ -7,14 +7,24 @@
 {{csrf_field()}}
 <!-- Para insertar un texto con un name= titulo -->
 <label for="titulo">Titulo:</label> <br/>
-<input type="text" name="titulo" class="input" value=""> <br/><br/>
+<input type="text" name="titulo" class="input @error('titulo') is-invalid @enderror" value="{{old('titulo')}}"> <br/><br/>
+@error('titulo')
+    <div class="alert alert-danger">{{$message}}</div>
+    <br/><br/>
+@enderror
 <!-- Para insertar un texto con un name descripcion -->
 <label for="descripcion">Descripcion:</label> <br/>
-<textarea name="descripcion" rows="5" class="form-control"></textarea>
+<textarea name="descripcion" rows="5" class="form-control @error('descripcion') is-invalid @enderror"></textarea>
+@error('descripcion')
+<div class="alert alert-danger">{{$message}}</div>
+@enderror
 <br/><br/>
 <!-- Para insertar un texto con un name cuerpo -->
 <label for="cuerpo">Cuerpo:</label><br/>
-<textarea name="cuerpo" rows="8" class="form-control"></textarea>
+<textarea name="cuerpo" rows="8" class="form-control @error('cuerpo') is-invalid @enderror"></textarea>
+@error('cuerpo')
+<div class="alert alert-danger">{{$message}}</div>
+@enderror
 <br /> <br />
 <button type="submit" class="btn btn-primary">Crear Articulo</button>
 </form>
