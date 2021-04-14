b4a48d229c8532867ffca59bd73e8216d7eba475
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-14 10:19:49 +0200

    :recycle: add articulos visualization in articulos view

--- jeff/proyectolaravel/resources/views/articulos/articulos.blade.php
+++ jeff/proyectolaravel/resources/views/articulos/articulos.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.header')
 @section('contenido')
-<h2>Bienvenido a la web sobre artículos</h2>
-<a href="{{route('insertar_articulo')}}" title="Añadir articulo">Añadir articulo</a>
-<p>Estos son los articulos publicados.</p>
+    <h2>Bienvenido a la web sobre artículos</h2>
+    <a href="{{route('insertar_articulo')}}" title="Añadir articulo">Añadir articulo</a>
+    <p>Estos son los articulos publicados.</p>
+    @foreach($articulos as $articulo)
+        <a href="{{ route('un_articulo', ['art'=>$articulo->id])}}" title="Ver articulo">
+            <h2>{{$articulo->titulo}}</h2>
+        </a>
+        <p>{{$fila->descripcion}}</p>
+        <br />
+    @endforeach
+
 @endsection
