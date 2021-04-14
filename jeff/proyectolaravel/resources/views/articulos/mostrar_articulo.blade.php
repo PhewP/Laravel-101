5858e6ab67ed1e75d037a5879e6a72cef528fbbd
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-14 10:44:00 +0200

    :sparkles: add mostrar_articulo view

---
+++ jeff/proyectolaravel/resources/views/articulos/mostrar_articulo.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.header')
+@section('contenido')
+    <a href="{{route('articulos')}}" title="Ver todos los articulos">Ver todos los articulos</a>
+    <p>Este es el articulo</p>
+
+    <h2>{{$articulo->titulo$}}</h2>
+    <h3>{{$articulo->descripcion}}</h3>
+    <p>{{$articulo->cuerpo</p>
+@endsection
