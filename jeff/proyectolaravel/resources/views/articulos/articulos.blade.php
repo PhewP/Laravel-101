61cecce8433dd5e6e782187e864828705f0de5ca
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-15 11:22:09 +0200

    :recycle: add user in articulos view if loged

--- jeff/proyectolaravel/resources/views/articulos/articulos.blade.php
+++ jeff/proyectolaravel/resources/views/articulos/articulos.blade.php
@@ -3,6 +3,11 @@
     <h2>Bienvenido a la web sobre artículos</h2>
     <a href="{{route('insertar_articulo')}}" title="Añadir articulo">Añadir articulo</a>
     <p>Estos son los articulos publicados.</p>
+
+    @if(Auth::check())
+        <button type="button" class="btn btn-primary">{{Auth::user()->name}}</button>
+        <a href="/user" title="Usuario">Usuario</a>
+    @endif
     @foreach($articulos as $articulo)
         <a href="{{ route('un_articulo', ['art'=>$articulo->id])}}" title="Ver articulo">
             <h2>{{$articulo->titulo}}</h2>
