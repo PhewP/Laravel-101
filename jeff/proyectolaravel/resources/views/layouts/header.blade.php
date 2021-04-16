b2e97cf04b0395af34ff35a0615176acdc5ffa94
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-16 15:56:53 +0200

    :bug: fix boostrap urls

--- jeff/proyectolaravel/resources/views/layouts/header.blade.php
+++ jeff/proyectolaravel/resources/views/layouts/header.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Titulo App Laravel 2021</title>
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <script src="js/bootstrap.bundle.js"></script>
+        <link href="{{url('css/bootstrap.css')}}"rel="stylesheet">
+        <script src="{{url('js/bootstrap.bundle.js')}}"></script>
     </head>
     <body>
         <h1>App Laravel 2021</h1>
