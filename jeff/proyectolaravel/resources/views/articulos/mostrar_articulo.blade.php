40905487a6143a88469774992105da37bd00375a
jeffresh <manuel.pallanunez@alum.uca.es>
2021-04-16 15:57:08 +0200

    :lipstick: add card styles to mostrar atriculos

--- jeff/proyectolaravel/resources/views/articulos/mostrar_articulo.blade.php
+++ jeff/proyectolaravel/resources/views/articulos/mostrar_articulo.blade.php
@@ -3,7 +3,13 @@
     <a href="{{route('articulos')}}" title="Ver todos los articulos">Ver todos los articulos</a>
     <p>Este es el articulo</p>
 
-    <h2>{{$articulo->titulo}}</h2>
-    <h3>{{$articulo->descripcion}}</h3>
-    <p>{{$articulo->cuerpo}}</p>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{$articulo->titulo}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Descripcion</h6>
+            <p class="card-text">{{$articulo->descripcion}}</p>
+            <h6 class="card-subtitle mb-2 text-muted">Cuerpo</h6>
+            <p class="card-text">{{$articulo->cuerpo}}</p>
+        </div>
+    </div>
 @endsection
